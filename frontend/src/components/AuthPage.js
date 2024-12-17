39844bf8da5c20ea2369736cde373a560364c9da
mesataksu <cmeaksu25@gmail.com>
2024-12-17 20:57:17 +0300

    front end

--- frontend/src/components/AuthPage.js
+++ frontend/src/components/AuthPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthForm from './AuthForm';
-import './AuthPage.css'
 
 const AuthPage = () => {
   const [isRegister, setIsRegister] = useState(false);
